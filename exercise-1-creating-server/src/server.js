beb285cb61be57c458732032c523e8b8c0a3d1b5
Michal Kapiczynski <kapiczynski.m@gmail.com>
2023-04-28 14:59:17 +0200

    Create exercise-1-creating-server

---
+++ exercise-1-creating-server/src/server.js
@@ -0,0 +1,25 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+(async () => {
+  //Create an express application
+  const app = express(); 
+  //default port to listen
+  const port = 8080; 
+  
+  //use middleware so post bodies 
+  //are accessable as req.body.{{variable}}
+  app.use(bodyParser.json()); 
+  app.use(express.urlencoded({ extended: true })) //for requests from forms-like data
+
+  // Root URI call
+  app.get( "/", ( req, res ) => {
+    res.status(200).send("Welcome to the Cloud!");
+  } );
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
