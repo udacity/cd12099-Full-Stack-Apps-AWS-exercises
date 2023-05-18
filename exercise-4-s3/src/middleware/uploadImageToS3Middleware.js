a02ad808dcd54095a9fdf34facd2f73c4abacfc4
Michal Kapiczynski <kapiczynski.m@gmail.com>
2023-05-18 17:03:33 +0200

    Improve Exercise 4- s3

--- exercise-4-s3/src/middleware/uploadImageToS3Middleware.js
+++ exercise-4-s3/src/middleware/uploadImageToS3Middleware.js
@@ -1,21 +1,19 @@
+import { S3Client } from "@aws-sdk/client-s3";
 import multer from "multer";
 import multerS3 from "multer-s3";
-import { S3Client } from "@aws-sdk/client-s3";
-
-const S3_BUCKET_NAME = "udacity-tweets-bucket";
 
 const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
         secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
     },
-    region: 'us-east-1'
+    region: process.env['AWS_REGION']
 })
 
 
 const s3Storage = multerS3({
     s3: s3,
-    bucket: S3_BUCKET_NAME, 
+    bucket: process.env['AWS_S3_BUCKET_NAME'], 
     metadata: (req, file, cb) => {
         cb(null, {fieldname: file.fieldname})
     },
