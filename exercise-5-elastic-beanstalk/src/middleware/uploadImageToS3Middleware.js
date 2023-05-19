e3a1f430af88ce952cc3c71c51a0c92438cd8420
Michal Kapiczynski <kapiczynski.m@gmail.com>
2023-05-19 11:39:31 +0200

    Update exercise-5

--- exercise-5-elastic-beanstalk/src/middleware/uploadImageToS3Middleware.js
+++ exercise-5-elastic-beanstalk/src/middleware/uploadImageToS3Middleware.js
@@ -2,8 +2,6 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 
-const S3_BUCKET_NAME = "udacity-tweets-bucket";
-
 const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
@@ -15,7 +13,7 @@ const s3 = new S3Client({
 
 const s3Storage = multerS3({
     s3: s3,
-    bucket: S3_BUCKET_NAME, 
+    bucket: process.env['AWS_S3_BUCKET_NAME'], 
     metadata: (req, file, cb) => {
         cb(null, {fieldname: file.fieldname})
     },
